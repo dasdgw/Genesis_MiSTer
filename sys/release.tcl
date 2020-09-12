c4997d9e49265709b9814df19fc399b0977dc78d
dasdgw <michifrank2@gmx.de>
2020-09-13 00:12:04 +0200

    reenable release script

--- sys/release.tcl
+++ sys/release.tcl
@@ -1,10 +1,10 @@
-# get current core by looking for .qpf file, but excluding Q13.qpf
-#set core [file rootname [lsearch -regexp -all -inline [glob *.qpf] "^((?!_Q13.qpf).)*$"] ]
-#set buildDate "[clock format [ clock seconds ] -format %Y%m%d]"
-#set rcIdx 1
-#while {[file exists releases/${core}_rc${rcIdx}_${buildDate}.rbf] != 0 } {
-#    incr rcIdx;
+# get current core by looking for .qpf file, but excluding _Q13.qpf
+set core [file rootname [lsearch -regexp -all -inline [glob *.qpf] "^((?!_Q13.qpf).)*$"] ]
+set buildDate "[clock format [ clock seconds ] -format %Y%m%d]"
+set rcIdx 1
+while {[file exists releases/${core}_rc${rcIdx}_${buildDate}.rbf] != 0 } {
+    incr rcIdx;
 #    puts $rcIdx
-#}
-#file copy -force output_files/${core}.rbf releases/${core}_rc${rcIdx}_${buildDate}.rbf
+}
+file copy -force output_files/${core}.rbf releases/${core}_rc${rcIdx}_${buildDate}.rbf
 
